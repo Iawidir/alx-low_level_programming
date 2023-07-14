f8a7d3e9dfe2dca52b1e558aa434839295988bff
Iawidir <iawidir@gmail.com>
2023-07-14 09:57:21 -0700

    a

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,19 +3,27 @@
 /**
  * main - Entry point
  *
- * Description: print all alphabet letters
+ * Description: print 0, 1, - 9
  *
  * Return: Always 0 (success)
 */
 
 int main(void)
 {
-        char ch = 'a';
-        while (ch <= 'z')
-        {
-                putchar(ch);
-                ch++;
-        }
-        putchar('\n');
-        return (0);
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
 }
