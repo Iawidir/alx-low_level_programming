0203fd9da39c38c2c770a7f7423579525b2df586
Iawidir <iawidir@gmail.com>
2023-07-24 10:52:45 -0700

    0x05

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,16 +7,16 @@
 */
 
 void puts_half(char *str)
-{       
-        int i;
-        
-        for (i = 0; str[i] != '\0'; i++)
-                ;
-        
-        i++;
-        for (i /= 2; str[i] != '\0'; i++)
-        {       
-                putchar(str[i]);
-        }       
-        putchar('\n');
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
 }
